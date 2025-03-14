d202d9b24671f92945857df9fc51e59df9e0e8a6
OliveiraPedro09 <pedromartinsoliveira9@gmail.com>
2025-03-14 20:24:01 -0300

    Fix: Atualização Modelo do Banco de Dados #1
    * Arrumados todos as tipagens de Datas para int para receber o unix timestamp
    * Padronização da data inicial para create_date
    * is_active recebe default e server_default para evitar valores indesejados
    * Mudança de classes para SnakeCase
    * Offset e Factor são opcionais
    * Na tabela ParameterType agora com comentario de mudança do nome do campo e pode vim um json vazio

--- models/db_model.py
+++ models/db_model.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
 from typing import Any
 
 from sqlalchemy import (
     BIGINT,
     JSON,
     Boolean,
-    DateTime,
     Float,
     ForeignKey,
     Integer,
@@ -30,12 +28,8 @@ class User(Base):
     name: Mapped[str] = mapped_column(String)
     email: Mapped[str] = mapped_column(String, unique=True)
     password: Mapped[str] = mapped_column(String)
-    initial_date: Mapped[datetime] = mapped_column(
-        DateTime, server_default=func.now()
-    )
-    last_update: Mapped[datetime] = mapped_column(
-        DateTime, server_default=func.now()
-    )
+    create_date: Mapped[int] = mapped_column(Integer, server_default=func.now())
+    last_update: Mapped[int] = mapped_column(Integer, server_default=func.now())
     old_password: Mapped[list[dict[str, Any]]] = mapped_column(
         JSON,
         server_default=text("'[]'::jsonb"),
@@ -49,35 +43,35 @@ class WeatherStation(Base):
     id: Mapped[int] = mapped_column(BIGINT, primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String)
     uid: Mapped[str] = mapped_column(String, unique=True)
-    adress: Mapped[list[str]] = mapped_column(
+    address: Mapped[list[str]] = mapped_column(
         JSON,
         server_default=text("'[]'::jsonb"),
         comment="List of address",
     )
     latitude: Mapped[float] = mapped_column(Float)
     longitude: Mapped[float] = mapped_column(Float)
-    initial_date: Mapped[datetime] = mapped_column(
-        DateTime, server_default=func.now()
-    )
-    last_date: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
-    last_update: Mapped[datetime] = mapped_column(
-        DateTime, server_default=func.now()
-    )
-    is_active: Mapped[bool] = mapped_column(Boolean, default=False)
+    create_date: Mapped[int] = mapped_column(Integer, server_default=func.now())
+    last_date: Mapped[int] = mapped_column(Integer, server_default=func.now())
+    last_update: Mapped[int] = mapped_column(Integer, server_default=func.now())
+    is_active: Mapped[bool] = mapped_column(Boolean, server_default=text("false"))
 
 
-class Parameter_Type(Base):
+class ParameterType(Base):
     __tablename__ = "parameter_types"
 
     id: Mapped[int] = mapped_column(BIGINT, primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String)
-    json: Mapped[dict[str, Any]] = mapped_column(JSON)
+    json: Mapped[dict[str, Any]] = mapped_column(  # Trocar o Nome do Campo depois
+        JSON, server_default=text("'{}'::jsonb")
+    )
     measure_unit: Mapped[str] = mapped_column(String)
     qnt_decimals: Mapped[int] = mapped_column(Integer)
-    offset: Mapped[float] = mapped_column(Float)
-    factor: Mapped[float] = mapped_column(Float)
-    initial_date: Mapped[str] = mapped_column(DateTime, server_default=func.now())
-    last_date: Mapped[str] = mapped_column(DateTime, server_default=func.now())
+    offset: Mapped[float | None] = mapped_column(Float)
+    factor: Mapped[float | None] = mapped_column(Float)
+    create_date: Mapped[int] = mapped_column(  # Não é necessario ?
+        Integer, server_default=func.now()
+    )
+    last_date: Mapped[int] = mapped_column(Integer, server_default=func.now())
 
 
 class Parameter(Base):
@@ -95,7 +89,7 @@ class Parameter(Base):
     parameter = relationship("Parameter_Type", back_populates="parameters")
 
 
-class Type_Alert(Base):
+class TypeAlert(Base):
     __tablename__ = "type_alerts"
 
     id: Mapped[int] = mapped_column(BIGINT, primary_key=True, index=True)
@@ -103,8 +97,8 @@ class Type_Alert(Base):
     name: Mapped[str] = mapped_column(String)
     value: Mapped[str] = mapped_column(String)
     math_signal: Mapped[str] = mapped_column(String)
-    initial_date: Mapped[str] = mapped_column(DateTime, server_default=func.now())
-    last_edit: Mapped[str] = mapped_column(DateTime, server_default=func.now())
+    create_date: Mapped[int] = mapped_column(Integer, server_default=func.now())
+    last_edit: Mapped[int] = mapped_column(Integer, server_default=func.now())
 
     parameter = relationship("Parameter", back_populates="type_alerts")
 
@@ -114,7 +108,7 @@ class Measures(Base):
 
     id: Mapped[int] = mapped_column(BIGINT, primary_key=True, index=True)
     value: Mapped[str] = mapped_column(String)
-    last_edit: Mapped[str] = mapped_column(DateTime, server_default=func.now())
+    measure_date: Mapped[int] = mapped_column(Integer, server_default=func.now())
 
     parameter_id: Mapped[int] = mapped_column(BIGINT, ForeignKey("parameters.id"))
 
@@ -127,7 +121,7 @@ class Alert(Base):
     id: Mapped[int] = mapped_column(BIGINT, primary_key=True, index=True)
     measure_id: Mapped[int] = mapped_column(BIGINT, ForeignKey("measures.id"))
     type_alert_id: Mapped[int] = mapped_column(BIGINT, ForeignKey("type_alerts.id"))
-    start_date: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now())
+    create_date: Mapped[int] = mapped_column(Integer, server_default=func.now())
 
     measure = relationship("Measures", back_populates="alerts")
     type_alerts = relationship("Type_Alert", back_populates="alerts")
